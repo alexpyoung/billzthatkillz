289761356bfb61d7066165ac83c0171d4da83437
Alex Young <alexyoung1992@gmail.com>
2020-04-20 16:49:22 -0400

    refactor(main): DRY and more pythonic style

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 from dataclasses import dataclass
 from datetime import datetime
 from glob import glob
+from itertools import chain
 from operator import itemgetter
 
 import pyocr
@@ -70,105 +71,73 @@ def process_jpgs(input_glob, output_dir):
             )
 
 
-def parse_chase_text(input_glob):
-    """
-    Returns List[Purchase]
-    """
+def find_purchases(input_glob, date_regex, price_regex):
+    patterns = {
+        "date": re.compile(date_regex),
+        "price": re.compile(price_regex),
+    }
+    files = glob(input_glob)
+    count = len(files)
+    for i, text_file in enumerate(files):
+        with open(text_file, "r") as text:
+            year = year_pattern.findall(text_file)[0]
+            logger.info("Parsing [%02d/%02d] %s", i + 1, count, text_file)
+            for line in text:
+                date_test = patterns["date"].findall(line)
+                price_test = patterns["price"].findall(line)
+                if date_test and price_test:
+                    yield [year, date_test, price_test, line]
+
+
+def find_chase_purchases(input_glob):
     date_regex = r"^([0-9]{2}\/[0-9]{2})"
     price_regex = r"\s([0-9]{1,5}\.[0-9]{2})$"
-    patterns = {
-        "date": re.compile(date_regex),
-        "price": re.compile(price_regex),
-    }
-    purchases = []
-    files = glob(input_glob)
-    count = len(files)
-    for i, text_file in enumerate(files):
-        with open(text_file, "r") as text:
-            year = year_pattern.findall(text_file)[0]
-            logger.info("Parsing [%d/%d] %s", i + 1, count, text_file)
-            for line in text:
-                date_test = patterns["date"].findall(line)
-                price_test = patterns["price"].findall(line)
-                if date_test and price_test:
-                    purchases.append(
-                        Purchase(
-                            datetime.strptime(
-                                f"{date_test[0]}/{year}", "%m/%d/%Y"
-                            ),
-                            re.sub(
-                                date_regex, "", re.sub(price_regex, "", line)
-                            ),
-                            price_test[0],
-                        )
-                    )
-    return purchases
+    for [year, date_test, price_test, line] in find_purchases(
+        input_glob, date_regex, price_regex
+    ):
+        yield Purchase(
+            datetime.strptime(f"{date_test[0]}/{year}", "%m/%d/%Y"),
+            re.sub(date_regex, "", re.sub(price_regex, "", line)),
+            price_test[0],
+        )
 
 
-def parse_barclays_text(input_glob):
-    """
-    Returns List[Purchase]
-    """
+def find_barclays_purchases(input_glob):
     date_regex = r"^([A-Z][a-z]{2}\s[0-9]{2})"
     price_regex = r"\s\$([0-9]{1,5}\.[0-9]{2})$"
-    patterns = {
-        "date": re.compile(date_regex),
-        "price": re.compile(price_regex),
-    }
-    purchases = []
-    files = glob(input_glob)
-    count = len(files)
-    for i, text_file in enumerate(files):
-        with open(text_file, "r") as text:
-            year = year_pattern.findall(text_file)[0]
-            logger.info("Parsing [%d/%d] %s", i + 1, count, text_file)
-            for line in text:
-                date_test = patterns["date"].findall(line)
-                price_test = patterns["price"].findall(line)
-                if date_test and price_test:
-                    vendor = re.sub(
-                        price_regex,
-                        "",
-                        re.sub(
-                            date_regex,
-                            "",
-                            re.sub(date_regex, "", line).strip(),
-                        ),
-                    )
-                    purchases.append(
-                        Purchase(
-                            datetime.strptime(
-                                f"{date_test[0]} {year}", "%b %d %Y"
-                            ),
-                            vendor,
-                            price_test[0],
-                        )
-                    )
-    return purchases
+    for [year, date_test, price_test, line] in find_purchases(
+        input_glob, date_regex, price_regex
+    ):
+        vendor = re.sub(
+            price_regex,
+            "",
+            re.sub(date_regex, "", re.sub(date_regex, "", line).strip()),
+        )
+        yield Purchase(
+            datetime.strptime(f"{date_test[0]} {year}", "%b %d %Y"),
+            vendor,
+            price_test[0],
+        )
 
 
 def cost_by_vendor(purchases):
-    """
-    Returns Dict[str, Money]
-    """
     unique_vendors = {}
     for purchase in purchases:
         subtotal = unique_vendors.get(
             purchase.vendor, Money(amount="0", currency=purchase.cost.currency)
         )
         unique_vendors[purchase.vendor] = subtotal + purchase.cost
-    return {v: c for v, c in sorted(unique_vendors.items(), key=itemgetter(1))}
+    for vendor, cost in sorted(unique_vendors.items(), key=itemgetter(1)):
+        yield vendor, cost
 
 
-def total_cost(purchases):
-    """
-    Returns Dict[str, Money]
-    """
+def cost_by_currency(purchases):
     total = {}
     for purchase in purchases:
         c = purchase.cost.currency
         total[c] = total.get(c, Money(amount="0", currency=c)) + purchase.cost
-    return total
+    for currency, cost in total.items():
+        yield currency, cost
 
 
 def main():
@@ -177,13 +146,16 @@ def main():
 
     process_pdfs(f"/opt/btk/pdf/*.pdf", temp_dir)
     process_jpgs(f"{temp_dir}/*.jpg", temp_dir)
-    purchases = parse_chase_text(f"{temp_dir}/chase*.txt")
-    purchases.extend(parse_barclays_text(f"{temp_dir}/barclays*.txt"))
-    vendors = cost_by_vendor(purchases)
-    for vendor, cost in vendors.items():
-        logger.info("%s %8.2f %s", cost.currency, cost.amount, vendor)
-    totals = total_cost(purchases)
-    for currency, total in totals.items():
+
+    purchases = list(
+        chain(
+            find_chase_purchases(f"{temp_dir}/chase*.txt"),
+            find_barclays_purchases(f"{temp_dir}/barclays*.txt"),
+        )
+    )
+    for vendor, cost in cost_by_vendor(purchases):
+        logger.info("%s %8.2f %s:", cost.currency, cost.amount, vendor)
+    for currency, total in cost_by_currency(purchases):
         logger.info("%s %8.2f ==∙∙=== TOTAL ===∙∙==", currency, total)
 
 
