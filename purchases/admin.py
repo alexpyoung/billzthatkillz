1a8b5bcfd880cfca8da9c27c637b4993c67dd4df
Alex Young <alexyoung1992@gmail.com>
2020-04-21 01:36:52 -0400

    feat(admin): add some nice admin views

--- purchases/admin.py
+++ purchases/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 
 from .models import Purchase, Vendor
 
-admin.site.register(Purchase)
-admin.site.register(Vendor)
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    readonly_fields = ("id", "date", "vendor", "amount", "currency")
+    date_hierarchy = "date"
+    list_display = ("id", "date", "vendor", "amount", "currency")
+    list_filter = ("date", "currency")
+    search_fields = ["vendor__name"]
+
+
+class VendorPurchaseAdmin(admin.TabularInline):
+    model = Purchase
+    fk_name = "vendor"
+    readonly_fields = ("id", "date", "vendor", "amount", "currency")
+    list_display = ("id", "date", "vendor", "amount", "currency")
+    can_delete = False
+
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    readonly_fields = ("id", "name")
+    list_display = ("id", "name")
+    list_filter = ["name"]
+    inlines = [VendorPurchaseAdmin]
--- purchases/models.py
+++ purchases/models.py
@@ -9,6 +9,9 @@ class Vendor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=2048)
 
+    def __str__(self):
+        return self.name
+
 
 class Purchase(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
