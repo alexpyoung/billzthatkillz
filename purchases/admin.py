5b3ef9d2c3720c3df94cc356612c99d97806e134
Alex Young <alexyoung1992@gmail.com>
2020-04-20 21:58:41 -0400

    feat(purchases): app and model

---
+++ purchases/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Purchase, Vendor
+
+admin.site.register(Purchase)
+admin.site.register(Vendor)
---
+++ purchases/models.py
@@ -0,0 +1,32 @@
+import re
+import uuid
+
+from django.db import models
+from money import Money
+
+
+class Vendor(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=2048)
+
+
+class Purchase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateTimeField()
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    currency = models.CharField(max_length=8)
+
+    @classmethod
+    def sanitize(cls, date, vendor_name, cost):
+        amount = Money(amount=cost, currency="USD")
+        (vendor, __) = Vendor.objects.get_or_create(
+            # Strip digits for better coalescing
+            name=re.sub(r"([0-9]+)", "", vendor_name.strip()),
+        )
+        return cls(
+            date=date,
+            vendor=vendor,
+            amount=amount.amount,
+            currency=amount.currency,
+        )
